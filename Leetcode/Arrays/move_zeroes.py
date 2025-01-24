d3c9a5ce81f34810f01fd98fe1a745968363249d
Prasad Aurangabadkar <prasadaurangabadkar@gmail.com>
2025-01-24 22:40:13 +0530

    comments addition

--- Leetcode/Arrays/move_zeroes.py
+++ Leetcode/Arrays/move_zeroes.py
@@ -1,9 +1,15 @@
 from typing import List
+
+
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+
+        # 1
+        """Brute force solution"""
+        """This was my thought process in the first go. It takes O(N^2) time to run"""
         # flag = True
         # for i in range(len(nums)-1, -1, -1):
         #     if nums[i] == 0 and flag:
@@ -18,29 +24,29 @@ class Solution:
         #     else:
         #         continue
         # return nums
-        #i, j = 0, 1
-        # while j <= len(nums) -1:
-        #     if nums[i] == 0 and nums[j] != 0:
-        #         nums[i], nums[j] = nums[j], nums[i]
-        #         i +=1
-        #         j +=1 
-        #     elif nums[i] == 0 and nums[j] == 0:
-        #         j += 1
-        #     else:
-        #         i +=1 
-        #         j+= 1
 
+        # 2
+        """A better way."""
+        """I came up with second soltion. In this I created two pointers and 
+        changed the slower one only when it was not equal to 0. Kind of close to ideal
+        solution but it has got some redundant conditions and higher space complexity.
+        Takes O(N) time but has high space complexity."""
 
+        # i, j = 0, 1
         # while j <= len(nums) -1:
         #     if nums[i] == 0 and nums[j] != 0:
         #         nums[i], nums[j] = nums[j], nums[i]
         #         i +=1
         #     elif (nums[i] != 0 and nums[j] != 0) or (nums[i] !=0 and nums[j] == 0):
-        #         i +=1 
-        #     j+=1 
+        #         i +=1
+        #     j+=1
         # return nums
+
+        # 3
+        """The ideal one.
+        Takes O(N) time. Sace complexity is low. """
         pos = 0
-        
+
         # First pass: place all non-zero elements
         for i in range(len(nums)):
             if nums[i] != 0:
@@ -48,13 +54,12 @@ class Solution:
                 pos += 1
 
 
-            
-                
-            
-
-
-
-
-
 s = Solution()
-print(s.moveZeroes([1,0,1]))
+print(s.moveZeroes([0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]))
+
+
+"""Learnings:
+1. Two pointer approach introduction.
+2. How two pointers can be used.
+3. Swapping in place as we did in last solution takes less space. """
